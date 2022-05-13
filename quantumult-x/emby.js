8ce73398d3e2a150a150951cba45d355d791597a
amwamw968 <270686859@qq.com>
2022-05-13 16:14:13 +0800

    Update emby.js

--- quantumult-x/emby.js
+++ quantumult-x/emby.js
@@ -18,13 +18,10 @@ if ($request.url.indexOf('/Users/') != -1) {
   if($util.status==200){
     //let body11 = JSON.stringify($response.body);
      console.log("$response.body:");
-    console.log($response.body);
-    $response.body = JSON.parse($response.body);
-    console.log("$response.body2:");
-     console.log($response.body);
+    console.log(JSON.stringify($response.body));
     $response.body = $response.body.replace(/"CanDownload":\s?false,/g, '"CanDownload": true,');
-    console.log("$response.body3:");
-     console.log($response.body);
+    console.log("$response.body2:");
+    console.log(JSON.stringify($response.body));
     let body = JSON.parse($response.body);
     let user_id_result = $request.url.match(/\/Users\/(\w{32})/);
     if (typeof(user_id_result) != "undefined") {
